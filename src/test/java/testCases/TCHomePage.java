8faa82507b2fbebf7d05249e1db68e0502bbfef5
jtmehul <mhlthkr@gmail.com>
2020-06-16 18:36:56 +0530

    added webdriverwait

--- src/test/java/testCases/TCHomePage.java
+++ src/test/java/testCases/TCHomePage.java
@@ -2,7 +2,11 @@ package testCases;
 
 import java.io.IOException;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import pageObjects.HomePage;
@@ -16,9 +20,12 @@ public class TCHomePage extends Base {
 	@Test
 	public void pageNavigation() throws IOException, InterruptedException{
 		driver = initBrowser();
+	
 		HomePage hp = new HomePage(driver);
-		Thread.sleep(5000);
-		//hp.getDestination().sendKeys("Mumbai");	
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='select2-drop']/div/input")));
+		hp.getDestination().sendKeys("mum");
+		hp.getDestination().sendKeys(Keys.ENTER);
 		hp.getCheckIn().click();
 	}
 }
